be6b3f5f12ed138f319eb734109d215edd2f2628
Angel Capa <ricardocmdn@gmail.com>
2024-12-05 11:24:13 -0500

    add changes

--- src/app/components/skeleton-home.tsx
+++ src/app/components/skeleton-home.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import Lottie from "lottie-react";
-import logoAnimation from "../../../public/lottie/motion_logo.json";
+// import logoAnimation from "../../../public/lottie/motion_logo.json";
 
 function Skeleton() {
   return (
     <div className="fixed z-[10000] bg-[#E4C042] h-[200vh] flex items-center justify-center">
       <div className="">
-        <Lottie
+        {/* <Lottie
           animationData={logoAnimation}
           loop={false}
           autoplay={true}
@@ -16,7 +16,7 @@ function Skeleton() {
             mixBlendMode: "multiply",
             background: "transparent",
           }}
-        />
+        /> */}
       </div>
     </div>
   );
