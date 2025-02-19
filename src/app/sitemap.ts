dee1d72574261ce983e216fa56aba145139f7dad
Angel Capa <ricardocmdn@gmail.com>
2025-02-19 16:24:07 -0500

    v2 sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,13 +6,15 @@ type Locale = "es" | "de" | "en";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://kuhlmann-partner.com";
   const languages: Locale[] = ["es", "en", "de"];
+  
+  // Rutas estáticas con prioridad asignada
   const staticRoutes = [
-    "/projects",
-    "/about-us",
-    "/news",
-    "/legal-notice",
-    "/privacy-policy",
-    "/cookies-policy",
+    { route: "/projects", priority: 0.8 },
+    { route: "/about-us", priority: 0.6 },
+    { route: "/news", priority: 0.7 },
+    { route: "/legal-notice", priority: 0.5 },
+    { route: "/privacy-policy", priority: 0.5 },
+    { route: "/cookies-policy", priority: 0.5 },
   ];
 
   const allProjectRoutesPromises = languages.map(async (lang) => {
@@ -29,6 +31,8 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return projectPages.map((project: { slug: string }) => ({
       url: `${baseUrl}/${lang}/projects/${project.slug}`,
       lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 0.6,  // Ajusta según el contexto
     }));
   });
 
@@ -48,21 +52,29 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return newsPages.map((news: { slug: string }) => ({
       url: `${baseUrl}/${lang}/news/${news.slug}`,
       lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 0.5,  // Ajusta según el contexto
     }));
   });
 
   const allNewsRoutes = (await Promise.all(allNewsRoutesPromises)).flat();
 
+  // Asignar prioridad a las rutas estáticas
   const allStaticRoutes = languages.flatMap((lang) =>
-    staticRoutes.map((route) => ({
+    staticRoutes.map(({ route, priority }) => ({
       url: `${baseUrl}/${lang}${route}`,
       lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority, // Asignar la prioridad que corresponde
     }))
   );
 
+  // Rutas principales con prioridad alta (1.0)
   const homeRoutes = languages.map((lang) => ({
     url: `${baseUrl}/${lang}`,
     lastModified: new Date(),
+    changeFrequency: "daily" as const,
+    priority: 1.0, // Alta prioridad para las páginas principales
   }));
 
   return [
