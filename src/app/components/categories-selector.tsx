0e49c7139d1d01de3df52ac1c94a54768b515ee3
Angel Capa <ricardocmdn@gmail.com>
2025-03-04 22:14:20 -0500

    router push

--- src/app/components/categories-selector.tsx
+++ src/app/components/categories-selector.tsx
@@ -8,23 +8,29 @@ interface CategoriesSelectorProps {
   locale: "es" | "en" | "de";
 }
 
-export const CategoriesSelector: React.FC<CategoriesSelectorProps> = ({ locale }) => {
+export const CategoriesSelector: React.FC<CategoriesSelectorProps> = ({
+  locale,
+}) => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const t = useTranslations();
 
   const categoriesByLanguage = {
     en: [
-      { id: 659, title: t("categories.new_construction") },
-      { id: 665, title: t("categories.reform") },
+      {
+        id: 659,
+        slug: "new-construction",
+        title: t("categories.new_construction"),
+      },
+      { id: 665, slug: "reform", title: t("categories.reform") },
     ],
     es: [
-      { id: 655, title: t("categories.new_construction") },
-      { id: 661, title: t("categories.reform") },
+      { id: 655, slug: "obra-nueva", title: t("categories.new_construction") },
+      { id: 661, slug: "reforma", title: t("categories.reform") },
     ],
     de: [
-      { id: 657, title: t("categories.new_construction") },
-      { id: 663, title: t("categories.reform") },
+      { id: 657, slug: "neubau", title: t("categories.new_construction") },
+      { id: 663, slug: "renovierung", title: t("categories.reform") },
     ],
   };
 
@@ -35,7 +41,10 @@ export const CategoriesSelector: React.FC<CategoriesSelectorProps> = ({ locale }
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
         setIsDropdownVisible(false);
       }
     };
@@ -57,8 +66,8 @@ export const CategoriesSelector: React.FC<CategoriesSelectorProps> = ({ locale }
             {categories.map((category) => (
               <Link
                 key={category.id}
-                locale={false} 
-                href={`/${locale}/projects?category=${category.id}`}
+                locale={false}
+                href={`/${locale}/projects?category=${category.slug}`}
                 className="whitespace-nowrap pr-[30px] cursor-pointer font-mediumFont tracking-[-0.01em] h-[40px] pl-[30px] w-auto flex items-center text-[20px] leading-[25px] hover:underline"
               >
                 {category.title}
--- src/app/components/projects-information.tsx
+++ src/app/components/projects-information.tsx
@@ -9,7 +9,7 @@ import { WordPressFrontendPage } from "../_interfaces/wordpress-page";
 import Items from "./items";
 import ProjectView from "./project-view";
 import { getUniqueCategories } from "../utils";
-import { usePathname, useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 
 interface ProjectsInformationDetails {
   information: DescriptionWp;
@@ -26,25 +26,24 @@ export function ProjectsInformation(props: ProjectsInformationDetails) {
   const categories = getUniqueCategories(allProjects);
   const t = useTranslations();
   const searchParams = useSearchParams();
-  const category = searchParams.get("category");
+  const router = useRouter();
+  const categorySlug = searchParams.get("category");
+  const selectedCategory = categories.find((cat) => cat.slug === categorySlug);
+  const selectedCategoryId = selectedCategory ? selectedCategory.id : -1;
 
   useEffect(() => {
-    console.log(category);
-    if (category) {
-      setSelectedOption(Number(category));
-    }
-  }, [category]);
+    setSelectedOption(selectedCategoryId);
+  }, [categorySlug]);
 
-  const handleClick = (id: number) => {
+  const handleClick = (id: number, slug: string) => {
     setSelectedOption(id);
-    // searchParams.set("category", id.toString());
+    window.history.pushState(null, "", `?category=${slug}`);
     const filterProjects = allProjects.filter((project) => {
       const categories = project._embedded["wp:term"].categories;
       return categories.find((category) => category.id === id);
     });
-    const results = id === -1 ? allProjects : filterProjects;
-    console.log(selectedOption);
 
+    const results = id === -1 ? allProjects : filterProjects;
     setIsFiltered(false);
     setTimeout(() => {
       setFilteredProjects(results);
@@ -53,18 +52,8 @@ export function ProjectsInformation(props: ProjectsInformationDetails) {
   };
 
   useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-out",
-      offset: 80,
-      once: false,
-    });
+    AOS.init({ duration: 1000, easing: "ease-out", offset: 80, once: false });
   }, []);
-
-  useEffect(() => {
-    setFilteredProjects(allProjects);
-    setIsFiltered(true);
-  }, [allProjects]);
 
   return (
     <div className="container">
@@ -86,6 +75,7 @@ export function ProjectsInformation(props: ProjectsInformationDetails) {
           }}
         />
       </section>
+
       <section data-aos="fade-up" className="pt-[30px] lg:pt-[80px]">
         <hr className="border-t border-black border-1 mb-[10px] lg:mb-[16px] hr-draw" />
         <div className="flex flex-col lg:grid lg:grid-cols-2">
@@ -108,28 +98,29 @@ export function ProjectsInformation(props: ProjectsInformationDetails) {
           </div>
         </div>
       </section>
+
       <section className="pt-[50px] lg:pt-[95px] pb-[50px] lg:pb-[0px]">
         <div data-aos="fade-up">
-          <div className="flex flex-col lg:flex-row lg:items-center gap-[26px] lg:gap-[14px] lg:gap-[11px] ">
+          <div className="flex flex-col lg:flex-row lg:items-center gap-[26px] lg:gap-[14px] lg:gap-[11px]">
             <p className="font-mediumFont text-[16px] leading-[16px] uppercase lg:text-[18px] lg:leading-[18px]">
-              {`${t("projects-page.projects")}`}:
+              {t("projects-page.projects")}:
             </p>
             <div className="w-full overflow-hidden">
               <div className="flex flex-row gap-[10px] overflow-x-scroll no-scrollbar">
                 <button
-                  onClick={() => handleClick(-1)}
+                  onClick={() => handleClick(-1, "all")}
                   className={`font-mediumFont uppercase hover:bg-black hover:text-white transition-colors duration-300 ease-in-out font-medium text-[15px] leading-[18px] lg:text-[18px] lg:leading-[18px] cursor-pointer border border-black h-[35px] px-[15px] ${
                     selectedOption === -1
                       ? "select-option rounded-full bg-black text-white"
                       : "text-black rounded-full"
                   } whitespace-nowrap min-w-[120px]`}
                 >
-                  {`${t("projects-page.all")}`}
+                  {t("projects-page.all")}
                 </button>
-                {categories.map((option, index) => (
+                {categories.map((option) => (
                   <button
-                    key={index}
-                    onClick={() => handleClick(option.id)}
+                    key={option.id}
+                    onClick={() => handleClick(option.id, option.slug)}
                     className={`font-mediumFont hover:bg-black hover:text-white transition-colors duration-300 ease-in-out font-medium text-[15px] leading-[18px] lg:text-[18px] lg:leading-[18px] cursor-pointer border border-black h-[35px] px-[15px] ${
                       selectedOption === option.id
                         ? "select-option rounded-full bg-black text-white"
@@ -143,6 +134,7 @@ export function ProjectsInformation(props: ProjectsInformationDetails) {
             </div>
           </div>
         </div>
+
         <div
           className={`transition-opacity duration-500 ease-in-out ${
             isFiltered ? "opacity-100" : "opacity-0"
