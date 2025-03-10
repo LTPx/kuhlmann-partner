2f299bf16050c89b1698c88bd7a4852dd0cc6ea7
Angel Capa <ricardocmdn@gmail.com>
2025-03-09 21:29:25 -0500

    add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,87 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/dienstleistungen-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/poolbau-schwimmbad-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/abbrucharbeiten-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/entsorgung-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/erdbewegungsarbeiten-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/einfriedungsarbeiten-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/rohbau-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/dachdecker-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/fliesenlegearbeiten-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/malerei-anstriche-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/verputzarbeiten-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/heizungs-energie-und-klimatechnik",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/sanitaer-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/heizungstechnik-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+      {
+        source: "/klimatechnik-mallorca",
+        destination: "/de",
+        permanent: true,
+      },
+    ];
+  },
+};
 
+const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
