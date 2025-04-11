3378ad635a90811776a5a3686387be76b5fe70c3
Angel Capa <ricardocmdn@gmail.com>
2025-04-11 12:41:05 -0500

    add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,151 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/es/default-5",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/ac1f5f65d1a9ffa18bd67288d9e32d90",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/a0777c9e6e8d9b0e9acc24de4c65ca36",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/6f86ce50a9deedfe50634c91b9e76c93",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/f6630ca246762826f17abf8ac7f03d04723112a5",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/a029ab2953edcfcf7a5921b69cfe0789",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/080284c9b5ec48709f9d8c31117c59fe",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/acbc9ac21a2d02852824cca18283080e",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/b1e348eafa6f6aee0089eb4f4c17f3f2",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/381b266f9faf8621a68b57298888f743",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/94b0300b1e864c38f97df780fb0b04ff",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/254f8f88c10eee1088c0285da64b186d",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/069efe9709f04ba086d629076fc9619e",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/c0a66891bb269501f15d049fd2840ed0",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/7b4f77e2ca8d17c634bf5746a1fabcbb",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/3542b98a92badfb2c71fb64343d24899",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/ab02ef6c2e1f7960b4d85ae42b5b2be5",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/costadenblanes-process",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/default-4",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/979816e5ec877c7ef7d360bdc11146e4",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/e37574f76fc7f608a4f2730ccea811ea",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/c410b2e8259047ba624c662091d44e7d",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/81bf96910b51cd421f1a3c7f72125ac6",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/481125602bcf4f367079db9f233bcd3d",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/54b305e1a52d9bbf0b5a4932503994b4",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/291a606a1f952ed292cbcd98c99d44ea",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/3b19eb9c7e8c545c325afa608783ea8e",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/14db5d2c8b7ae7093984fcd4282ca53b",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/llucmajor-web09",
+        destination: "/es/projects/llucmajor",
+        permanent: true,
+      },      
+      {
         source: "/news/blog-1",
         destination: "/de/news",
         permanent: true,
@@ -15,7 +160,7 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/es/author/claudia-kuhlmann/page/2/",
+        source: "/es/author/claudia-kuhlmann/page/2",
         destination: "/es",
         permanent: true,
       },
@@ -50,7 +195,7 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/es/blogthemen/page/2/",
+        source: "/es/blogthemen/page/2",
         destination: "/es/news",
         permanent: true,
       },
@@ -65,8 +210,18 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/es/kp-lucmajor-photos-3-2/",
+        source: "/es/kp-lucmajor-photos-3-2",
         destination: "/es/projects/llucmajor",
+        permanent: true,
+      },
+      {
+        source: "/es/8959175a7edad4039d2c090b55dcdb27",
+        destination: "/es",
+        permanent: true,
+      },
+      {
+        source: "/es/304ad0ee6a2a689b4af7dcf84d6730f5",
+        destination: "/es",
         permanent: true,
       },
       {
